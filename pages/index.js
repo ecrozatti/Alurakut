8d135249c7e86d817ca2dead87e651872ccba910
Eric Crozatti <eric.crozatti@gmail.com>
2021-07-18 22:41:13 -0300

    Commons commit

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 
-export default function Home() {
-  return (
-     <MainGrid>
-         <div className='profileArea' style={{ gridArea: 'profileArea' }}>
-            <Box>
-               Imagem
-            </Box>
-            <img src="https:///github.com/ecrozatti.png" alt="Imagem GitHub" />
-        </div>
+function ProfileSidebar(props) {
+   return(
+      <Box>
+         <img src={`https:///github.com/${props.githubUser}.png`} alt="Imagem GitHub" style={{ borderRadius: '8px' }}/>
+      </Box>
+   );
+}
 
-        <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
+export default function Home() {
+   const githubUser = 'ecrozatti';
+
+   return (
+      <MainGrid>
+         <div className='profileArea' style={{ gridArea: 'profileArea' }}>
+            {/* <ProfileSidebar githubUser={githubUser} /> */}
+         </div>
+
+         <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
             <Box>
                Bem vindo
             </Box>
-        </div>
+         </div>
 
-        <div className='profileRelationsArea' style={{ gridArea: 'profileRelationsArea' }}>
+         <div className='profileRelationsArea' style={{ gridArea: 'profileRelationsArea' }}>
             <Box>
-            Comunidades
+               Comunidades
             </Box>
-        </div>
-     </MainGrid>
-  )
+         </div>
+      </MainGrid>
+   )
 }
